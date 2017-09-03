ac9f1fbf71b0e7a731755a71ad3a95986069e1a5
anton <antoni-fox@yandex.ru>
2017-09-03 16:29:34 +0300

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace RockScissorsPaper
+{
+    public static class Game
+    {
+        public enum Gestures
+        {
+            Rock = 0,
+            Scissors = 1,
+            Paper = 2
+        }
+
+        private static readonly Random Random = new Random();
+        public static Gestures RandGesture => GetRandomEnum<Gestures>();
+
+        private static T GetRandomEnum<T>()
+        {
+            Array values = Enum.GetValues(typeof(T));
+            T randomEnumType = (T) values.GetValue(Random.Next(values.Length));
+            return randomEnumType;
+        }
+
+        public static byte ChooseWinner(Gestures firstGesture, Gestures secondGesture)
+        {
+            if ((firstGesture == Gestures.Rock && secondGesture == Gestures.Scissors) ||
+                (firstGesture == Gestures.Scissors && secondGesture == Gestures.Paper) ||
+                (firstGesture == Gestures.Paper && secondGesture == Gestures.Rock))
+            {
+                return 1;
+            }
+
+            if ((secondGesture == Gestures.Rock && firstGesture == Gestures.Scissors) ||
+                (secondGesture == Gestures.Scissors && firstGesture == Gestures.Paper) ||
+                (secondGesture == Gestures.Paper && firstGesture == Gestures.Rock))
+            {
+                return 2;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace RockScissorsPaper
+{
+    internal class Program
+    {
+        enum GameOtions
+        {
+            GamerWithComputer = 1,
+            TwoGamers = 2,
+            TwoComupters = 3,
+            Quit = 4
+        }
+
+        public static void Main(string[] args)
+        {
+            byte optionNumber = 0;
+            while (optionNumber != 4)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Выберите действие:\n" +
+                                  "1. Игрок против компьютера\n" +
+                                  "2. Игрок против игрока\n" +
+                                  "3. Компьютера против компьютеа\n" +
+                                  "4. Выход");
+                Console.ForegroundColor = ConsoleColor.Green;
+                String consoleKeyInfo = Console.ReadLine();
+
+                try
+                {
+                    if (consoleKeyInfo != null)
+                    {
+                        optionNumber = Byte.Parse(consoleKeyInfo);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0}: Неверный формат\n", consoleKeyInfo);
+                    continue;
+                }
+                catch (OverflowException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0}: Слишком большое число\n", consoleKeyInfo);
+                    continue;
+                }
+
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                switch ((GameOtions) optionNumber)
+                {
+                    case GameOtions.GamerWithComputer:
+                        break;
+                    case GameOtions.TwoGamers:
+                        break;
+                    case GameOtions.TwoComupters:
+                        GameWithTwoComputers();
+                        break;
+                    case GameOtions.Quit:
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Неверный номер\n");
+                        break;
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public static void GameWithComputer()
+        {
+        }
+
+        public static void GameWithTwoGamers()
+        {
+        }
+
+        public static void GameWithTwoComputers()
+        {
+            var firstGesture = Game.RandGesture;
+            var secondGesture = Game.RandGesture;
+            CheckGameWinner(firstGesture, secondGesture);
+        }
+
+
+        public static void CheckGameWinner(Game.Gestures firstGesture, Game.Gestures secondGesture)
+        {
+            Console.WriteLine("Игрок 1 выбрал: {0} \nИгрок 2 выбрал: {1}", firstGesture.ToString(),
+                secondGesture.ToString());
+            var resultOfGame = Game.ChooseWinner(firstGesture, secondGesture);
+            Console.WriteLine(((resultOfGame == 1) || (resultOfGame == 2))
+                ? ("Выиграл игрок " + resultOfGame)
+                : ("Ничья"));
+        }
+    }
+}
